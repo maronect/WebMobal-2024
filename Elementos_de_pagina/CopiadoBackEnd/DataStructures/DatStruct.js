a208ee3f908b593e00d545bd3168c7f22ff5a7ff
Marone Taques <maronect@gmail.com>
2024-04-07 20:13:47 -0300

    Passando codigo do Ubuntu para o Windows

---
+++ Elementos_de_pagina/CopiadoBackEnd/DataStructures/DatStruct.js
@@ -0,0 +1,13 @@
+const array = []
+
+let pegandoPl = document.getElementById("pl");
+let pegandoFl = document.getElementById("fl");
+let add = document.getElementById('add')
+let rem = document.getElementById('rem')
+let clear = document.getElementById('clear')
+
+function adicionar(num){
+    array.push(num)
+}
+
+
