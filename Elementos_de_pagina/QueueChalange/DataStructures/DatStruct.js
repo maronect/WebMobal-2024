f81c426f25bd27c284890873121833c6f649dc8b
Marone Taques <maronect@gmail.com>
2024-04-12 00:12:43 -0300

    Desafio concluido

--- Elementos_de_pagina/QueueChalange/DataStructures/DatStruct.js
+++ Elementos_de_pagina/QueueChalange/DataStructures/DatStruct.js
@@ -1,21 +1,35 @@
 let array = []
-let num = document.getElementById("num");
-
-let pegandoFl = document.getElementById("fl");
-let add = document.getElementById('add')
-let rem = document.getElementById('remove')
-let clear = document.getElementById('clear')
-
 
 function adicionar(){
-    array.push((num));
-    
-    document.getElementById("vet").textContent = array;
-
+    let nomeElement = document.getElementById("nome").value;
+    let corElemet = document.getElementById("cor").value;
+    let valores = {
+        nome: nomeElement,
+        cor:  corElemet
+    };
+    array.push(valores);
+    arrayBuild();
+    document.getElementById("nome").value = "";
+    document.getElementById("cor").value = "";
 }
 
 function concluir (){
     array.pop();
-    document.getElementById("vet").textContent = array;
+    arrayBuild();
+}
 
+function arrayBuild (){
+    document.getElementById('vet').innerHTML = '';
+    for(let i = 0; i < array.length; i++) {
+        let ballNum = document.createElement('div');
+        let box = document.createElement('span');
+        
+        box.innerHTML = array[i].nome;       
+        ballNum.style.background = array[i].cor;
+        ballNum.classList.add("numberBal");
+        ballNum.innerHTML = i+1;
+
+        box.appendChild(ballNum);
+        document.getElementById('vet').appendChild(box);
+    }
 }
